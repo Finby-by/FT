4f94c04978674b4f71b1e841bb67280f76e9a206
Nikolay Kurtov <nkurtov@ulvetanna.io>
2024-06-26 23:44:07 +0200

    Add methods: * ft_burn * new_meta_bnd * new_meta_shr * set_icon_bnd * set_icon_shr

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -23,7 +23,7 @@ use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
-use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
+use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue, require};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -33,6 +33,8 @@ pub struct Contract {
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const BND_SVG_ICON: &str = "data:image/svg+xml,<%3Fxml version='1.0' encoding='utf-8'%3F><svg fill='%23000000' width='800px' height='800px' viewBox='0 0 24 24' id='up-trend' data-name='Line Color' xmlns='http://www.w3.org/2000/svg' class='icon line-color'><polyline id='primary' points='21 6 14 13 11 10 3 18' style='fill: none; stroke: rgb(0, 0, 0); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;'></polyline><polyline id='secondary' points='21 10 21 6 17 6' style='fill: none; stroke: rgb(102,202,60); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;'></polyline></svg>";
+const SHR_SVG_ICON: &str = "data:image/svg+xml,<%3Fxml version='1.0' encoding='utf-8'%3F><svg fill='%23000000' width='800px' height='800px' viewBox='0 0 24 24' id='up-trend' data-name='Line Color' xmlns='http://www.w3.org/2000/svg' class='icon line-color'><polyline id='primary' points='21 6 14 13 11 10 3 18' style='fill: none; stroke: rgb(0, 0, 0); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;'></polyline><polyline id='secondary' points='21 10 21 6 17 6' style='fill: none; stroke: rgb(218,84,220); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;'></polyline></svg>";
 const ERR_TOTAL_SUPPLY_OVERFLOW: &str = "Total supply overflow";
 
 #[near_bindgen]
@@ -52,6 +54,56 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
                 decimals: 24,
+            },
+        )
+    }
+
+    pub fn set_icon_bnd(&mut self) {
+        let predecessor = env::predecessor_account_id();
+        require!(predecessor == "finby.near".parse().unwrap(), "Unauthorized");
+        let mut metadata = self.metadata.get().expect("Metadata not set");
+        metadata.icon = Some(BND_SVG_ICON.to_string());
+        self.metadata.set(&metadata);
+    }
+
+    pub fn set_icon_shr(&mut self) {
+        let predecessor = env::predecessor_account_id();
+        require!(predecessor == "finby.near".parse().unwrap(), "Unauthorized");
+        let mut metadata = self.metadata.get().expect("Metadata not set");
+        metadata.icon = Some(SHR_SVG_ICON.to_string());
+        self.metadata.set(&metadata);
+    }
+
+    #[init]
+    pub fn new_meta_bnd(owner_id: AccountId, total_supply: U128) -> Self {
+        Self::new(
+            owner_id,
+            total_supply,
+            FungibleTokenMetadata {
+                spec: FT_METADATA_SPEC.to_string(),
+                name: "Finby BND".to_string(),
+                symbol: "BND".to_string(),
+                icon: Some(BND_SVG_ICON.to_string()),
+                reference: None,
+                reference_hash: None,
+                decimals: 0,
+            },
+        )
+    }
+
+    #[init]
+    pub fn new_meta_shr(owner_id: AccountId, total_supply: U128) -> Self {
+        Self::new(
+            owner_id,
+            total_supply,
+            FungibleTokenMetadata {
+                spec: FT_METADATA_SPEC.to_string(),
+                name: "Finby SHR".to_string(),
+                symbol: "SHR".to_string(),
+                icon: Some(SHR_SVG_ICON.to_string()),
+                reference: None,
+                reference_hash: None,
+                decimals: 0,
             },
         )
     }
